d0123b346f1308bda07aa5a45ed87e26b3abdae8
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-17 22:52:54 +0530

    handled signup page

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,16 +9,24 @@ const handleSignUp=async(req,res)=>{
    try{
     validateSignUpData(req);
 
-    let {email}=req.body;
+    let {email,password,name}=req.body;
+    
 
     const registeredUser=await User.find({email:email});
     
     if(registeredUser.length>0) throw new Error("User already registered");
 
+    const hashedPassword=await bcrypt.hash(password,10);
+
+    const user=new User({
+        name,email,password:hashedPassword
+    });
+    
+    await user.save();
 
     res.status(201).json({
         result:"success",
-        message:"done successfully"
+        message:"user saved successfully"
     })
      
       
