8ed4e67bcbc53e3d8579e2142da0d43ebe049cea
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-17 21:50:53 +0530

    connectiong to database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,30 @@
 require("dotenv").config();
 const express =require("express");
+const { connectDB } = require("./config/database");
 const app=express();
 const port=process.env.PORT
 
 
-app.listen(port,()=>{
-    console.log(`app listening on port ${port}`);
+
+
+
+
+
+
+
+
+
+
+connectDB().then(()=>{
+    console.log("connected to db succesully");
+    app.listen(port,()=>{
+        console.log(`app listening on port ${port}`);
+    })
+
+
 })
+.catch((err)=>{
+  console.log(err);
+})
+
+
