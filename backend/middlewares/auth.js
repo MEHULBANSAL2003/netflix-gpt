564849116cf7ef4d27a29842ecaa380fb0b76c00
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-22 13:56:02 +0530

    minor change

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require("jsonwebtoken");
-const {User} = require("../model/user.js");
+const { User } = require("../model/user.js");
 
 const userAuth = async (req, res, next) => {
   try {
     const cookies = req.cookies;
 
     const { token } = cookies;
-    //console.log(token);
 
     if (!token) {
       throw new Error("invalid token");
@@ -15,23 +14,20 @@ const userAuth = async (req, res, next) => {
     const validateToken = await jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     const { _id } = validateToken;
-    console.log(_id);
 
     const user = await User.findById(_id);
-    console.log(user);
-    if (!user) {
 
+    if (!user) {
       throw new Error("user not found. Login to access");
     }
-   
-  
-    req.user = user; 
+
+    req.user = user;
     next();
   } catch (err) {
     res.status(400).json({
-      result:"error",
-      message:err.message
-    })
+      result: "error",
+      message: err.message,
+    });
   }
 };
 
