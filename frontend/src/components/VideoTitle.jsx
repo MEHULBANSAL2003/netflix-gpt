2f74548e206c400701e46b5f3ee9ff84d2e2e7bd
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-23 20:22:15 +0530

    styled button

--- frontend/src/components/VideoTitle.jsx
+++ frontend/src/components/VideoTitle.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { FaPlay } from "react-icons/fa";
+import { MdInfoOutline } from "react-icons/md";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div style={{height:"90vh"}} className="w-screen pt-[15%] px-12 absolute text-white bg-gradient-to-r from-black">
+    <div
+      style={{ height: "90vh" }}
+      className="w-screen pt-[15%] px-12 absolute text-white bg-gradient-to-r from-black"
+    >
       <h1 className="text-6xl font-bold">{title}</h1>
       <h2 className="py-6 text-lg w-1/4">{overview}</h2>
 
-      <div>
-        <button className="bg-white text-black p-3 px-12 text-xl rounded hover:bg-opacity-80">
-          Play
+      <div className="flex">
+        <button className="flex items-center bg-white text-black p-3 px-10 text-xl rounded hover:bg-opacity-80">
+          <FaPlay className="mr-2" /> Play
         </button>
-        <button className="mx-3 bg-gray-600 text-white p-3 px-8 text-xl rounded-lg hover:bg-opacity-50">
-          More Info
+        <button className="flex items-center mx-3 bg-gray-600 text-white p-3 px-5 text-xl rounded hover:bg-opacity-50">
+          <MdInfoOutline className="mr-2" /> More Info
         </button>
       </div>
     </div>
