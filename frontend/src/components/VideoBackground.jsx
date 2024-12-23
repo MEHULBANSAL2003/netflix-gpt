5902300d412ccdc3fc691e70126171362bd0da25
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-23 18:10:22 +0530

    added custom hook for showing video trailor

--- frontend/src/components/VideoBackground.jsx
+++ frontend/src/components/VideoBackground.jsx
@@ -1,37 +1,11 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utilities/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailorVideo } from "../redux/movieSlice";
+import React from "react";
+import {  useSelector } from "react-redux";
+import useTrailorVideo from "../hooks/useTrailorVideo";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
+  useTrailorVideo(movieId);
 
   const trailorId = useSelector((store) => store.movies?.trailorVideo);
-
-  const getMovieVideo = async () => {
-    const data = await axios(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-
-    const json = data?.data;
-
-    const filteredTrailerVideos = json.results.filter((item) => {
-      return item.type === "Trailer";
-    });
-
-    const trailorVideo = filteredTrailerVideos.length
-      ? filteredTrailerVideos[0]
-      : json.results[0];
-
-    //console.log(trailorVideo);
-    dispatch(addTrailorVideo(trailorVideo));
-  };
-
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
 
   return (
     <div>
---
+++ frontend/src/hooks/useTrailorVideo.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { useDispatch} from 'react-redux';
+import { API_OPTIONS } from '../utilities/constants';
+import { addTrailorVideo } from '../redux/movieSlice';
+import axios from 'axios';
+const useTrailorVideo = (movieId) => {
+    const dispatch = useDispatch();
+
+ 
+
+  const getMovieVideo = async () => {
+    const data = await axios(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    );
+
+    const json = data?.data;
+
+    const filteredTrailerVideos = json.results.filter((item) => {
+      return item.type === "Trailer";
+    });
+
+    const trailorVideo = filteredTrailerVideos.length
+      ? filteredTrailerVideos[0]
+      : json.results[0];
+
+    //console.log(trailorVideo);
+    dispatch(addTrailorVideo(trailorVideo));
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+}
+
+export default useTrailorVideo
