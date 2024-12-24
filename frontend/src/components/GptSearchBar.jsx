7096ec23c7f3080b0d3c1beda62a622b48427b79
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-25 00:08:01 +0530

    minor change

--- frontend/src/components/GptSearchBar.jsx
+++ frontend/src/components/GptSearchBar.jsx
@@ -12,7 +12,7 @@ const GptSearchBar = () => {
     <div className='pt-[10%] flex justify-center'>
       
 
-      <form className=' w-1/2 bg-black grid grid-cols-12'>
+      <form className=' w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
        
        <input type="text" className='p-4 m-4 col-span-9' placeholder={lang[currLang].gptSearchPlaceholder} />
        <button className='py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded'>{lang[currLang].search}</button>
