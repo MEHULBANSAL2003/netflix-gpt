1192bc89bb36c50adcd4e54da34ae2b657f3635d
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-25 00:11:46 +0530

    minor chnage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -45,9 +45,13 @@ const Header = () => {
     dispatch(changeLang(e.target.value));
   }
 
+  const handleLogoClick=()=>{
+  navigate('/browse');
+  }
+
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex items-center justify-between w-full">
-      <img className="w-44" src={NETFLIX_LOGO} alt="Netflix Logo" />
+      <img className="w-44 hover:cursor-pointer" src={NETFLIX_LOGO} alt="Netflix Logo" onClick={handleLogoClick} />
       <div className="relative flex items-center space-x-4">
           
          {userInfo.email && <select className=" p-2 m-2 bg-gray-950 text-white border border-black" onChange={handleLanguageChange} >
