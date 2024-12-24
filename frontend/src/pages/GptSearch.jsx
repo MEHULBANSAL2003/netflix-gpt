2ae4af2b61d2d99a4502f8e9aa25f7f9219bc828
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-24 23:14:48 +0530

    developed gptSearchBar

--- frontend/src/pages/GptSearch.jsx
+++ frontend/src/pages/GptSearch.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from '../components/Header'
 import GptSearchBar from '../components/GptSearchBar'
 import GptMovieSuggestion from '../components/GptMovieSuggestion'
+import { BG_IMAGE } from '../utilities/constants'
 
 const GptSearch = () => {
   return (
     <div>
         <Header/>
+       <div className='absolute -z-10'>
+        <img src={BG_IMAGE} alt="background imagw" />
+
+       </div>
         <GptSearchBar/>
         <GptMovieSuggestion/>
+        
 
     </div>
   )
