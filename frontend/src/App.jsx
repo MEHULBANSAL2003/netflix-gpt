e48d7dea03e4e51ef70d0a1622c996fdd708ef9d
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-28 01:27:24 +0530

    added loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,24 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import React, { useEffect, Suspense, lazy } from "react";
+import React, { useEffect, useState, Suspense, lazy } from "react";
 import axios from "axios";
 import { setUser } from "./redux/userSlice";
+import Loader from "./components/Loader";
 
 const Login = lazy(() => import("./pages/Login"));
 const Home = lazy(() => import("./pages/Home"));
 const Browse = lazy(() => import("./pages/Browse"));
 const ChangePassword = lazy(() => import("./pages/ChangePassword"));
-const GptSearch=lazy(()=>import("./pages/GptSearch"))
+const GptSearch = lazy(() => import("./pages/GptSearch"));
 
 function App() {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   const getUser = async () => {
+    setLoading(true);
     try {
       const URL = `${import.meta.env.VITE_BACKEND_URL}/getUserByToken`;
       const response = await axios({
@@ -24,7 +27,7 @@ function App() {
         url: URL,
         withCredentials: true,
       });
-      if (response.data.result == "success") {
+      if (response.data.result === "success") {
         if (!user.email) {
           const name = response.data.data.name;
           const email = response.data.data.email;
@@ -32,7 +35,9 @@ function App() {
         }
       }
     } catch (err) {
-      console.log(err);
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -42,36 +47,34 @@ function App() {
 
   return (
     <div>
-      {/* <Login/>
-     <ToastContainer />  */}
-      <Suspense
-        fallback={
-          <div className="text-center font-bold text-5xl">Loading...</div>
-        }
-      >
-        <Routes>
-          <Route
-            path={"/"}
-            element={!user.email ? <Home /> : <Navigate to="/browse" />}
-          />
-          <Route
-            path={"/login"}
-            element={!user.email ? <Login /> : <Navigate to="/browse" />}
-          />
-          <Route
-            path={"/browse"}
-            element={user.email ? <Browse /> : <Navigate to="/login" />}
-          />
-          <Route
-            path={"/change-password"}
-            element={user.email ? <ChangePassword /> : <Navigate to="/login" />}
-          />
-           <Route
-            path={"/gpt-search"}
-            element={user.email ? <GptSearch /> : <Navigate to="/login" />}
-          />
-        </Routes>
-      </Suspense>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path={"/"}
+              element={!user.email ? <Home /> : <Navigate to="/browse" />}
+            />
+            <Route
+              path={"/login"}
+              element={!user.email ? <Login /> : <Navigate to="/browse" />}
+            />
+            <Route
+              path={"/browse"}
+              element={user.email ? <Browse /> : <Navigate to="/login" />}
+            />
+            <Route
+              path={"/change-password"}
+              element={user.email ? <ChangePassword /> : <Navigate to="/login" />}
+            />
+            <Route
+              path={"/gpt-search"}
+              element={user.email ? <GptSearch /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </Suspense>
+      )}
 
       <ToastContainer />
     </div>
