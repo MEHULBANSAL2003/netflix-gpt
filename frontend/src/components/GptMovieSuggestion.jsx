119b7a79863746f878d77864bce6ab786eeb0e48
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-28 01:06:21 +0530

    minor changes

--- frontend/src/components/GptMovieSuggestion.jsx
+++ frontend/src/components/GptMovieSuggestion.jsx
@@ -6,9 +6,6 @@ const GptMovieSuggestion = () => {
   
   const {movieResults,movieNames}=useSelector((store)=>store.gpt);
 
-  if(!movieNames) return null;
-
-
 
   return (
     <div className='p-4 m-4 bg-black text-white bg-opacity-80'>
