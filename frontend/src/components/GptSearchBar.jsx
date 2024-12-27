f6d3b410022a1041d6772ac84c00863f02b4f952
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-27 19:23:55 +0530

    used openai api to request movie dta

--- frontend/src/components/GptSearchBar.jsx
+++ frontend/src/components/GptSearchBar.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import lang from '../utilities/languageConstants'
-import { useSelector } from 'react-redux'
-
-
+import React, { useRef } from "react";
+import lang from "../utilities/languageConstants";
+import { useSelector } from "react-redux";
+import openai from "../utilities/openai";
 
 const GptSearchBar = () => {
+  const currLang = useSelector((store) => store.lang.lang);
 
-  const currLang=useSelector((store)=>store.lang.lang);
+  const searchText = useRef(null);
+
+  const searchQuery = `Act as a movie recommendation system and suggest some movies for the query : "${searchText.current.value}".Only give me name of 10 movies,comma separated like the example result given ahead. Example Result: Gadar,sholay,don, golmal...`;
+
+  const handleGptSearch = async () => {
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: searchQuery }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(gptResults.choices[0].message.content);
+  };
 
   return (
-    <div className='pt-[10%] flex justify-center'>
-      
-
-      <form className=' w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
-       
-       <input type="text" className='p-4 m-4 col-span-9' placeholder={lang[currLang].gptSearchPlaceholder} />
-       <button className='py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded'>{lang[currLang].search}</button>
-
+    <div className="pt-[10%] flex justify-center">
+      <form
+        className=" w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          ref={searchText}
+          type="text"
+          className="p-4 m-4 col-span-9"
+          placeholder={lang[currLang].gptSearchPlaceholder}
+        />
+        <button
+          onClick={handleGptSearch}
+          className="py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded"
+        >
+          {lang[currLang].search}
+        </button>
       </form>
-      
     </div>
-  )
-}
+  );
+};
 
-export default GptSearchBar
+export default GptSearchBar;
