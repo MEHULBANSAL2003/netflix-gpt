652c48f71d3b299edf300febc286539375a77d9c
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-27 23:19:34 +0530

    handled error case

--- frontend/src/components/GptSearchBar.jsx
+++ frontend/src/components/GptSearchBar.jsx
@@ -2,19 +2,34 @@ import React, { useRef } from "react";
 import lang from "../utilities/languageConstants";
 import { useSelector } from "react-redux";
 import openai from "../utilities/openai";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const GptSearchBar = () => {
   const currLang = useSelector((store) => store.lang.lang);
+  const navigate=useNavigate();
 
   const searchText = useRef(null);
 
-  const searchQuery = `Act as a movie recommendation system and suggest some movies for the query : "${searchText.current.value}".Only give me name of 10 movies,comma separated like the example result given ahead. Example Result: Gadar,sholay,don, golmal...`;
-
+  
+  
   const handleGptSearch = async () => {
+    const searchQuery = `Act as a movie recommendation system and suggest some movies for the query : "${searchText?.current?.value}".Only give me name of 10 movies,comma separated like the example result given ahead. Example Result: Gadar,sholay,don, golmal...`;
+ 
     const gptResults = await openai.chat.completions.create({
       messages: [{ role: "user", content: searchQuery }],
       model: "gpt-3.5-turbo",
     });
+
+    console.log(gptResults);
+
+    if(gptResults.choices[0].message.content.includes("movie recommendations")||gptResults.choices[0].message.content.includes("movies")){
+      toast.error("Sorry..!! No movie found.");
+      searchText.current.value="";
+        navigate("/gpt-search");
+
+      
+    }
 
     console.log(gptResults.choices[0].message.content);
   };
